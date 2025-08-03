aebf9389bbbc198aba1475df5565228ed804dc61
francescogiga <146665445+francescogiga@users.noreply.github.com>
2025-08-03 19:45:36 +0200

    Add files via upload

---
+++ index6.php
@@ -0,0 +1,19 @@
+<?php
+
+for ($i = 1; $i <= 100; $i++) {
+    switch($i) {
+        case $i % 15 == 0:
+            echo "HACKADEMY \n";
+            break;
+        case $i % 3 ==0:
+            echo "PHP \n";
+            break;
+        case $i % 5 == 0:
+                echo "JS \n";
+                break;
+         
+            default:
+            echo "$i \n";
+            break;
+    }
+}
